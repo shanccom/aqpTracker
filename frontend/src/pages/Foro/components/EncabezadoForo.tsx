c2fe3dacf941f3286e90550b1c86673b3d0bc5eb
Julio Chura <jchuraaca@unsa.edu.pe>
2025-11-11 15:47:47 -0500

    se borra el icono de barra de busqueda

--- frontend/src/pages/Foro/components/EncabezadoForo.tsx
+++ frontend/src/pages/Foro/components/EncabezadoForo.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Search } from 'lucide-react'
 
 const EncabezadoForo: React.FC = () => {
   return (
@@ -9,12 +8,7 @@ const EncabezadoForo: React.FC = () => {
           <div className="flex items-center gap-4 text-gray-900">
             <h2 className="text-xl font-bold leading-tight tracking-tight">Incidentes Recientes</h2>
           </div>
-          <div className="flex flex-1 justify-end gap-2">
-            <button className="flex items-center justify-center rounded-lg h-10 w-10 bg-gray-100 text-gray-600 hover:bg-gray-200">
-              <Search size={18} />
-            </button>
-            <div className="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 h-10 border border-gray-300" style={{backgroundImage: `url('/static/img/profile.jpg')`}} />
-          </div>
+          
         </div>
       </div>
     </header>
